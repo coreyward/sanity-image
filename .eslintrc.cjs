599ecfcfb930b09658d271066a38ce20f95e5c8f
Corey Ward <corey@hey.com>
2023-03-09 14:41:43 -0600

    Update eslint configuration for TS awareness

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,21 @@
 module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
   extends: [
+    "eslint:recommended",
     "standard",
     "plugin:react/recommended",
     "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/recommended",
   ],
-  plugins: ["standard", "react", "react-hooks", "prettier"],
+  plugins: ["react", "react-hooks", "prettier", "@typescript-eslint"],
   rules: {
+    "no-use-before-define": "off",
+    "no-unused-vars": "warn",
     "react/prop-types": "warn",
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn",
-    "no-unused-vars": "warn",
+    "@typescript-eslint/consistent-type-imports": "warn",
   },
   settings: {
     react: {
