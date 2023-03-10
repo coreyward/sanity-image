d5c2f9f737df23f7c1d50879a8551fe187541bb3
Corey Ward <corey@hey.com>
2023-03-10 14:44:16 -0600

    Add test for parseImageId

---
+++ src/parseImageId.test.ts
@@ -0,0 +1,29 @@
+import { parseImageId } from "./parseImageId"
+
+describe("parseImageId", () => {
+  it("parses image id", () => {
+    expect(parseImageId("image-abcd1234-100x2000-jpg")).toEqual({
+      assetId: "abcd1234",
+      dimensions: { height: 2000, width: 100, aspectRatio: 0.05 },
+      format: "jpg",
+    })
+  })
+
+  it("throws an error if the image id is invalid", () => {
+    expect(() => {
+      parseImageId("image-abcd1234-100x2000")
+    }).toThrowError('Could not parse image ID "image-abcd1234-100x2000"')
+    expect(() => {
+      parseImageId("image-abcd1234-100x2000-")
+    }).toThrowError('Could not parse image ID "image-abcd1234-100x2000-"')
+    expect(() => {
+      parseImageId("abcd1234-100x2000.jpg")
+    }).toThrowError('Could not parse image ID "abcd1234-100x2000.jpg"')
+  })
+
+  it("throws an error if the dimensions are invalid", () => {
+    expect(() => {
+      parseImageId("image-abcd1234-100x0-jpg")
+    }).toThrowError('Invalid dimensions "100x0"')
+  })
+})
