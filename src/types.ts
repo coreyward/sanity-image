d7a8b7129d2e4f53f70bcef142aa6c80ac49ff31
Corey Ward <corey@hey.com>
2023-03-10 20:40:00 -0600

    Refactor types and give credit

--- src/types.ts
+++ src/types.ts
@@ -7,50 +7,14 @@ import {
   type SanityImageSource,
   type Orientation,
 } from "@sanity/image-url/lib/types/types"
-
-// Source: https://github.com/emotion-js/emotion/blob/master/packages/styled-base/types/helper.d.ts
-// A more precise version of just React.ComponentPropsWithoutRef on its own
-export type PropsOf<
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  C extends keyof JSX.IntrinsicElements | React.JSXElementConstructor<any>
-> = JSX.LibraryManagedAttributes<C, React.ComponentPropsWithoutRef<C>>
-
-type AsProp<C extends React.ElementType> = {
-  /**
-   * By default, the component will render an `<img>` tag. You can override this
-   * by passing a different component or HTML tag name.
-   */
-  as?: C
-}
-
-/**
- * Allows for extending a set of props (`ExtendedProps`) by an overriding set of props
- * (`OverrideProps`), ensuring that any duplicates are overridden by the overriding
- * set of props.
- */
-export type ExtendableProps<
-  ExtendedProps = object,
-  OverrideProps = object
-> = OverrideProps & Omit<ExtendedProps, keyof OverrideProps>
-
-/**
- * Allows for inheriting the props from the specified element type so that
- * props like children, className & style work, as well as element-specific
- * attributes like aria roles. The component (`C`) must be passed in.
- */
-export type InheritableElementProps<
-  C extends React.ElementType,
-  Props = object
-> = ExtendableProps<PropsOf<C>, Props>
-
-/**
- * A more sophisticated version of `InheritableElementProps` where
- * the passed in `as` prop will determine which props can be included
- */
-export type PolymorphicComponentProps<
-  C extends React.ElementType,
-  Props = object
-> = InheritableElementProps<C, Props & AsProp<C>>
+import type { AsProp } from "./polymorphic-voodoo"
+export type {
+  AsProp,
+  PropsOf,
+  ExtendableProps,
+  InheritableElementProps,
+  PolymorphicComponentProps,
+} from "./polymorphic-voodoo"
 
 export type SanityImageProps = ImageQueryInputs & {
   preview?: string
