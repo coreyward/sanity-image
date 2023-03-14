554fd0956a96e5c7f4b620980f13bafae8c85bf9
Corey Ward <corey@hey.com>
2023-03-14 17:31:08 -0500

    Fix indendentation in dist specific package.json

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -1,11 +1,11 @@
 cat >dist/cjs/package.json <<!EOF
 {
-    "type": "commonjs"
+  "type": "commonjs"
 }
 !EOF
 
 cat >dist/mjs/package.json <<!EOF
 {
-    "type": "module"
+  "type": "module"
 }
 !EOF
